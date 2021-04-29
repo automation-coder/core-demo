c881a43530254a2b816e6a4eb366f7c9f89e1f1e
gcb5773 <Debojyoti.Bhattacharya@varian.com>
2021-04-29 10:31:27 +0530

    First commit for .net core project

---
+++ CoreDemo/API/Request/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CoreDemo.API.Request
+{
+    class API
+    {
+        private HttpClient httpClient = new HttpClient();
+        private string URI = "https://api.publicapis.org";
+
+        public async Task<string> testrequest()
+        {
+            var Builder = new System.UriBuilder($"{URI}/entries?category=animals&https=true");
+            var response = await httpClient.GetAsync(Builder.Uri);
+            var context = await response.Content.ReadAsStringAsync();
+            return context;
+        }
+    }
+}
---
+++ CoreDemo/Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Tests
+{
+    public class Tests
+    {
+        [Category("Test")]
+        [Test]
+        public async Task testApiCall()
+        {
+            CoreDemo.API.Request.API api = new CoreDemo.API.Request.API();
+            var response = await api.testrequest();
+        }
+    }
+}
